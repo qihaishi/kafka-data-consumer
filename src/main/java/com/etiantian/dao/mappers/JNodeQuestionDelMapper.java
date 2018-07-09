6a0278828d48db580ff51d01b47d0b3922d06627
祁海石 <qihaishi@etiantian.com>
2018-07-09 20:25:39 +0800

    注入beanName处理

--- src/main/java/com/etiantian/dao/mappers/JNodeQuestionDelMapper.java
+++ src/main/java/com/etiantian/dao/mappers/JNodeQuestionDelMapper.java
@@ -2,9 +2,12 @@ package com.etiantian.dao.mappers;
 
 import com.etiantian.entity.JNodeQuestionDel;
 import com.etiantian.entity.JNodeQuestionDelExample;
-import java.util.List;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
+
+@Component("jNodeQuestionDelMapper")
 public interface JNodeQuestionDelMapper {
     int countByExample(JNodeQuestionDelExample example);
 
