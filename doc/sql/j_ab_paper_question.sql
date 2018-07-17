b501137265f3cf695d511c377aee87b7cca2c105
kangxuefeng <kangxuefeng@etiantian.com>
2018-07-17 14:48:16 +0800

    添加索引

--- doc/sql/j_ab_paper_question.sql
+++ doc/sql/j_ab_paper_question.sql
@@ -3,37 +3,18 @@
 -- drop index IDX_J_AB_PAPER_QUESTION_GGS_TIME on j_ab_paper_question;
 
 drop table if exists j_ab_paper_question;
-
-/*==============================================================*/
-/* Table: j_ab_paper_question                                   */
-/*==============================================================*/
-create table j_ab_paper_question
-(
-   ref                  bigint(19) not null auto_increment comment '自增主键',
-   node_id              bigint(19) not null comment '节点id',
-   paper_id             bigint(19) not null comment '试卷id',
-   paper_type           char(1) not null comment '试卷类型:A/B',
-   question_id          bigint(19) not null comment '试题Id',
-   sort                 bigint(2) comment '排序号',
-   is_direct            bigint(2) default 0 comment '是否直属,0:否1:是',
-   ggs_time             timestamp not null default CURRENT_TIMESTAMP,
-   primary key (ref)
-);
-
-alter table j_ab_paper_question comment '节点,ab卷和试题关系表';
-
-/*==============================================================*/
-/* Index: IDX_J_AB_PAPER_QUESTION_GGS_TIME                      */
-/*==============================================================*/
-create index IDX_J_AB_PAPER_QUESTION_GGS_TIME on j_ab_paper_question
-(
-   ggs_time
-);
-
-/*==============================================================*/
-/* Index: IDX_J_AB_PAPER_QUESTION_PAPER_ID                      */
-/*==============================================================*/
-create index IDX_J_AB_PAPER_QUESTION_PAPER_ID on j_ab_paper_question
-(
-   paper_id
-);
+CREATE TABLE `j_ab_paper_question` (
+  `ref` bigint(19) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+  `node_id` bigint(19) NOT NULL COMMENT '节点id',
+  `paper_id` bigint(19) NOT NULL COMMENT '试卷id',
+  `paper_type` char(1) NOT NULL COMMENT '试卷类型:A/B',
+  `question_id` bigint(19) NOT NULL COMMENT '试题Id',
+  `sort` bigint(2) DEFAULT NULL COMMENT '排序号',
+  `is_direct` bigint(2) DEFAULT '0' COMMENT '是否直属,0:否1:是',
+  `ggs_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ref`),
+  KEY `IDX_J_AB_PAPER_QUESTION_GGS_TIME` (`ggs_time`),
+  KEY `IDX_J_AB_PAPER_QUESTION_PAPER_ID` (`paper_id`),
+  KEY `IDX_J_AB_PAPER_QUESTION_NODE_ID` (`node_id`),
+  KEY `IDX_J_AB_PAPER_QUESTION_QUES_ID` (`question_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='节点,ab卷和试题关系表'
