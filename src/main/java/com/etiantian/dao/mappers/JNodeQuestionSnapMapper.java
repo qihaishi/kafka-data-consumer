7f59ee61da65c4ec020a95d09da8b4e771c412a7
mengxianjuan <mengxianjuan@etiantian.com>
2018-08-13 12:06:35 +0800

    ZIXUE-5855 增加注解

--- src/main/java/com/etiantian/dao/mappers/JNodeQuestionSnapMapper.java
+++ src/main/java/com/etiantian/dao/mappers/JNodeQuestionSnapMapper.java
@@ -4,7 +4,9 @@ import com.etiantian.entity.JNodeQuestionSnap;
 import com.etiantian.entity.JNodeQuestionSnapExample;
 import java.util.List;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
 
+@Component("jNodeQuestionSnapMapper")
 public interface JNodeQuestionSnapMapper {
     int countByExample(JNodeQuestionSnapExample example);
 
--- src/main/java/com/etiantian/dao/mappers/JNodeSortSnapMapper.java
+++ src/main/java/com/etiantian/dao/mappers/JNodeSortSnapMapper.java
@@ -4,7 +4,9 @@ import com.etiantian.entity.JNodeSortSnap;
 import com.etiantian.entity.JNodeSortSnapExample;
 import java.util.List;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
 
+@Component("jNodeSortSnapMapper")
 public interface JNodeSortSnapMapper {
     int countByExample(JNodeSortSnapExample example);
 
