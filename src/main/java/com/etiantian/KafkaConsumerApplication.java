8e1432f597286d86d88b1868b743493a8d152807
祁海石 <qihaishi@etiantian.com>
2018-08-20 16:58:46 +0800

    消费者换写法

--- src/main/java/com/etiantian/KafkaConsumerApplication.java
+++ src/main/java/com/etiantian/KafkaConsumerApplication.java
@@ -1,22 +1,14 @@
 package com.etiantian;
 
-import com.etiantian.launcher.SpringContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
-@EnableKafka
-@Import(value={SpringContext.class})
-@ComponentScan(value = "com.etiantian")
+@ImportResource("classpath:spring/applicationContext.xml")
 public class KafkaConsumerApplication {
-
 	public static void main(String[] args) {
-		new ClassPathXmlApplicationContext("spring/applicationContext.xml");
 		SpringApplication.run(KafkaConsumerApplication.class, args);
 	}
 }
